fc286592057874a610841131bba02e9df925ccb7
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-01-28 07:33:50 +0700

    Demo buoi 02

---
+++ Lab01/Lab01/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(double SoHang01, double SoHang02, string ToanTu)
+        {
+            double KetQua = 0;
+            switch(ToanTu)
+            {
+                case "+": KetQua = SoHang01 + SoHang02; break;
+                case "^": KetQua = Math.Pow(SoHang01, SoHang02); break;
+                case "%": KetQua = SoHang01 % SoHang02; break;
+            }
+
+            //truyền dữ liệu qua View Index.cshtml để hiển thị
+            ViewBag.SoHang1 = SoHang01;
+            ViewBag.SoHang2 = SoHang02;
+            ViewBag.KetQua = KetQua;
+            ViewBag.PhepToan = ToanTu;
+            return View("Index");
+        }
+    }
+}
---
+++ Lab01/Lab01/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Lab01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>
+        {
+            new Product{Id=1, Name = "Bún Bò", Price = 39000},
+            new Product{Id=2, Name = "7 up", Price = 10900},
+            new Product{Id=3, Name = "Bánh canh", Price = 39000},
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            var product = products.SingleOrDefault(p => p.Id == model.Id);
+            if (product == null)
+            {
+                products.Add(model);
+
+                //lưu file
+                var json = System.Text.Json.JsonSerializer.Serialize(products);
+                System.IO.File.WriteAllText("Product.json", json);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Message = $"Mã {model.Id} đã có.";
+            return View(model);
+        }
+    }
+}
