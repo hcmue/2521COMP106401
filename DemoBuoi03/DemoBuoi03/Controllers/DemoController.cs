44dff744b9f429719e0901a11d9b939022afa679
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-02-03 08:10:42 +0700

    Demo Async + Upload file

---
+++ DemoBuoi03/DemoBuoi03/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using DemoBuoi03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoBuoi03.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult DemoSync()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Demo.A();
+            Demo.B();
+            Demo.C();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+
+        public async Task<IActionResult> DemoAS()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var a = Demo.AA();
+            var b = Demo.BB();
+            var c = Demo.CC();
+            await a; await b; await c;
+            //Task.WaitAll();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+    }
+}
---
+++ DemoBuoi03/DemoBuoi03/Controllers/UploaderController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace DemoBuoi03.Controllers
+{
+    public class UploaderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile myfile)
+        {
+            if (myfile == null)
+            {
+                ViewBag.Message = "Chưa chọn file upload.";
+            }
+            else
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", myfile.FileName);
+                using (var f = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    myfile.CopyTo(f);
+                }
+            }
+            return View("Index");
+        }
+
+        public IActionResult UploadFiles(List<IFormFile> myfiles)
+        {
+            if (myfiles == null || myfiles.Count == 0)
+            {
+                ViewBag.Message = "Chưa chọn file upload.";
+            }
+            else
+            {
+                foreach (var myfile in myfiles)
+                {
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", myfile.FileName);
+                    using (var f = new FileStream(fullPath, FileMode.CreateNew))
+                    {
+                        myfile.CopyTo(f);
+                    }
+                }
+            }
+            return View("Index");
+        }
+    }
+}
