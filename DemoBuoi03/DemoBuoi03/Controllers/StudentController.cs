9aa1995b08bc4977ff5b3706b2ca9958fd60d85b
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-02-03 08:31:11 +0700

    Demo Student

---
+++ DemoBuoi03/DemoBuoi03/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using DemoBuoi03.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace DemoBuoi03.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model, IFormFile Hinh)
+        {
+            if (Hinh != null)
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", Hinh.FileName);
+                using (var f = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    Hinh.CopyTo(f);
+                    model.Image = Hinh.FileName;
+                }
+            }
+            return View("Profile", model);
+        }
+
+
+    }
+}
---
+++ DemoBuoi03/DemoBuoi03/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBuoi03.Models
+{
+    public class Student
+    {
+        [RegularExpression(@"\d{2}.01.104.\d{3}")]
+        public string Id { get; set; }
+
+        [MinLength(5, ErrorMessage ="Tối thiểu 5 kí tự")]
+        public string Name { get; set; }
+        public string? Image { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Điểm từ 0 .. 10")]
+        public double Score { get; set; }
+    }
+}
