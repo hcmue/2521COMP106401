e5d134bb05c294fa680ad072dee5f9b7ac449bcf
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-03-10 08:47:10 +0700

    Partial View, View Component

---
+++ DemoLayout/DemoLayout/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DemoLayout.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLayout.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //đọc database
+            var danhMuc = new List<Category>
+            {
+                new Category{Id=1, Name="Điện thoại"},
+                new Category{Id=2, Name="Máy tính bảng"},
+                new Category{Id=3, Name="Điều hòa"},
+            };
+            return View(danhMuc);
+        }
+    }
+}
